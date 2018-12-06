93b9ae72ff481eec2a6c55011bedf9b66736b145
Avanade <Avanade@WIN-12C61DRULX.corp.avanade.org>
2018-12-06 14:55:53 -0200

    AuthGuard, menu arrumado e botao de sair funfando

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,32 @@ import { LoginComponent } from './components/login/login.component';
 import { HomeComponent } from './components/home/home.component';
 import { ExtratoComponent } from './components/extrato/extrato.component';
 import { TransferenciasComponent } from './components/transferencias/transferencias.component';
+import { AuthGuard } from './guards/auth.guard';
+import { MenuComponent } from './components/menu/menu.component';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
 
 export const routes: Routes = [
-  {path: '', component: LoginComponent},
-  {path: 'home', component: HomeComponent},
-  {path: 'extrato', component: ExtratoComponent},
-  {path: 'transferencias', component: TransferenciasComponent},
+  {
+    path: '',
+    component: LoginComponent
+  },
+  { 
+    path: 'home', 
+    component: HomeComponent,
+    canActivate: [AuthGuard]
+  },
+  { 
+    path: 'extrato', 
+    component: ExtratoComponent,
+    canActivate: [AuthGuard]
+  },
+  { 
+    path: 'transferencias', 
+    component: TransferenciasComponent,
+    canActivate: [AuthGuard]
+  },
+
 ];
 
 @NgModule({
---
+++ src/app/guards/auth.guard.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(private authService: AuthService,
+    private router: Router) { }
+
+  canActivate(): boolean {
+    if (this.authService.usuarioEstaLogado()) {
+      console.log('true')
+      return true
+    } else {
+      console.log('false')            
+      this.router.navigate(['/'])
+      return false
+    }
+  }
+}
